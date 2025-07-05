698812e39429411f3d918870f489b3db92aa9edd
Souvik Sardar <souviksardar16@gmail.com>
2025-07-06 01:53:46 +0530

    Added credits and howto page

--- Assets/Sandbox/Scripts/HomeSceneManager.cs
+++ Assets/Sandbox/Scripts/HomeSceneManager.cs
@@ -9,25 +9,31 @@ namespace SandboxGame
     {
         public Button BuildButton;
         public Button SettingsButton;
+        public Button HowtoButton;
+        public Button CreditsButton;
         public PNL_Settings SettingsPanel;
+        public GameObject HowToPanel;
+        public GameObject CreditsPanel;
 
 
         private void Awake()
         {
             BuildButton.onClick.AddListener(OnBuildButtonClicked);
             SettingsButton.onClick.AddListener(OnSettingsButtonClicked);
+            HowtoButton.onClick.AddListener(OnHowtoButtonClicked);
+            CreditsButton.onClick.AddListener(OnCreditsButtonClicked);
         }
 
         // Start is called before the first frame update
         void Start()
         {
-        
+
         }
 
         // Update is called once per frame
         void Update()
         {
-        
+
         }
 
         void OnBuildButtonClicked()
@@ -39,6 +45,16 @@ namespace SandboxGame
         void OnSettingsButtonClicked()
         {
             SettingsPanel.gameObject.SetActive(true);
+        }
+
+        void OnHowtoButtonClicked()
+        {
+            HowToPanel.SetActive(true);
+        }
+
+        void OnCreditsButtonClicked()
+        {
+            CreditsPanel.SetActive(true);
         }
 
     }
